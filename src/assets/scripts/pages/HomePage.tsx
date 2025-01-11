257da65200b7df1e2c9d2724ec82003084cbee35
Yevhenii Klyman <kliman.evgeniy@gmail.com>
2025-01-11 18:07:05 +0200

    Dropdown, loader improvement

--- src/assets/scripts/pages/HomePage.tsx
+++ src/assets/scripts/pages/HomePage.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { useLazyGetUserReposQuery, useSearchUsersQuery } from '../store/github/github.api'
 import { useDebounce } from '../hooks/useDebounce';
 import { RepoCard } from '../components/RepoCard';
@@ -12,13 +12,30 @@ export function HomePage() {
     skip: debounced.length < 3,
     refetchOnFocus: true
   })
+  const inputRef = useRef<HTMLInputElement>(null);
+  const dropdownRef = useRef<HTMLUListElement>(null);
 
   const [fetchRepos, { isFetching, data: repos }] = useLazyGetUserReposQuery()
 
-  const clickHandler = (username: string) => {
+  const userClickHandler = (username: string) => {
     fetchRepos(username)
     setIsDropdownVisible(false)
   }
+
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (dropdownRef.current && !dropdownRef.current.contains(event.target as Node) &&
+        inputRef.current && !inputRef.current.contains(event.target as Node)) {
+        setIsDropdownVisible(false)
+      }
+    };
+
+    document.addEventListener('click', handleClickOutside);
+
+    return () => {
+      document.removeEventListener('click', handleClickOutside);
+    };
+  }, []);
 
   useEffect(() => {
     setIsDropdownVisible(debounced.length > 3 && !!data?.length)
@@ -33,19 +50,23 @@ export function HomePage() {
           className="border border-slate-300 py-2 px-4 w-full h-[45px] mb-2"
           placeholder="Search for GitHub username..."
           value={search}
+          ref={inputRef}
           onChange={e => setSearch(e.target.value)}
         />
-        {isDropdownVisible && <ul className="list-none absolute top-[45px] left-0 right-0 max-h-[200px] overflow-y-scroll shadow-md bg-white">
-          {isLoading && <Loader size={8} wrapcClasses='h-12' />}
+        {isDropdownVisible && <ul
+          className="list-none absolute top-[45px] left-0 right-0 border border-slate-300 max-h-[200px] overflow-y-scroll shadow-2xl bg-white"
+          ref={dropdownRef}
+        >
+          {isLoading && <Loader size='small' wrapcClasses='h-12' />}
           {data?.map(user => (
             <li
               key={user.id}
-              onClick={() => clickHandler(user.login)}
+              onClick={() => userClickHandler(user.login)}
               className="py-2 px-4 hover:bg-gray-500 hover:text-white transition-colors cursor-pointer"
             >{user.login}</li>
           ))}
         </ul>}
-        {isFetching && <Loader size={10} wrapcClasses='mt-5' />}
+        {isFetching && <Loader size='large' wrapcClasses='mt-5' />}
         {!isFetching && repos?.map(repo => <RepoCard repo={repo} key={repo.id} />)}
         <div>
         </div>
